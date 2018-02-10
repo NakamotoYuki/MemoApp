ebc031d13d7a758d510600ac79e47c36905b1a29
nkmt <nfmail9@gmail.com>
2018-02-11 00:37:51 +0900

    add remote save function

--- App.js
+++ App.js
@@ -4,9 +4,10 @@ import firebase from 'firebase';
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
-
+require("firebase/firestore");
 import ENV from './env.json';
 
 const config = {
@@ -25,6 +26,7 @@ const App = StackNavigator({
     Home: {screen: MemoListScreen},
     MemoDetail: {screen: MemoDetailScreen},
     MemoEdit: {screen: MemoEditScreen},
+    MemoCreate: {screen: MemoCreateScreen},
     },{
         navigationOptions: {
            headerTitle: 'Memot',
