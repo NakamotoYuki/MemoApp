bd7efff384ebb38c64b3946a231184027f68d557
nkmt <nfmail9@gmail.com>
2018-10-08 15:29:06 +0900

    途中

--- src/screens/MemoDetailScreen.js
+++ src/screens/MemoDetailScreen.js
@@ -18,6 +18,10 @@ class MemoDetailScreen extends React.Component {
         this.setState({ memo: params.memo });
     }
 
+  returnMemo(memo){
+    this.setState({ memo });
+  }
+
     render() {
         const { memo } = this.state;
         return (
@@ -40,8 +44,8 @@ class MemoDetailScreen extends React.Component {
 				<CircleButon 
                     color="white" 
                     style={styles.editButton} 
-                    onPress={() => { this.props.navigation.navigate('MemoEdit', { memo })}}
-                >
+                    onPress={() => { this.props.navigation.navigate('MemoEdit', { memo, returnMemo: this.returnMemo.bind(this) });}}
+          >
                     {'\uf040'}
                 </CircleButon>
 			</View>
--- src/screens/MemoEditScreen.js
+++ src/screens/MemoEditScreen.js
@@ -11,22 +11,30 @@ class MemoEditScreen extends React.Component {
 
     componentWillMount() {
         const { params } = this.props.navigation.state;
-        this.setState({ 
+        this.setState({
             body: params.memo.body,
-            key: params.memo.key
+            key: params.memo.key,
         });
     }
 
     handlePress() {
         const { currentUser } = firebase.auth();
         const db = firebase.firestore();
+        const newDate = new Date();
         console.log(this.state);
         db.collection(`users/${currentUser.uid}/memos`).doc(this.state.key)
             .update({
                 body: this.state.body,
+                createdOn: newDate,
             })
             .then(() => {
-                console.log('success');
+              const { navigation } = this.props;
+              navigation.state.params.returnMemo({
+                body: this.state.body,
+                key: this.state.key,
+                createdON: newDate,
+              });
+              navigation.goBack();
             })
             .catch((error) => {
                 console.log(error);
